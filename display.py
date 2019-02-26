0c5b7e3d1eb1735d7cd00f25d19fe4bf86ecc082
Theodore Peters <tpeters@stuy.edu>
2019-02-26 16:38:59 -0500

    first steps

---
+++ display.py
@@ -0,0 +1,86 @@
+import pygame
+import pygame.locals
+
+from player import Player, binput
+from helpers import *
+from animate import Animate
+
+def get_textures(filename):
+    m = pygame.image.load("textures/" + filename + ".png")
+    mw, mh = m.get_size()
+    textures = []
+    for i in range(0, mh, 32):
+        for j in range(0, mw, 32):
+            textures.append(m.subsurface(j, i, 32, 32))
+    return textures
+walls = get_textures("walls")
+door = get_textures("door")
+player = get_textures("pp_hed")
+dictadd = ('a', 'm')
+animdict = { i: get_textures(i) for i in dictadd }
+
+def render_wall(p, r, c, s, x, y):
+    for i in range(4):
+        if 0 <= r + rr(i,0) < p.level.r and 0 <= c + rr(i, 1) < p.level.c and p.level.dungeon[r + rr(i,0)][c + rr(i,1)] != 0 and p.fow[r + (i-1 if i % 2 == 0 else 0)][c + (i-2 if i % 2 == 1 else 0)]:
+            s.blit(pygame.transform.rotate(walls[1], 90 * i - 90), (x, y))
+    for i in range(4):
+        if 0 <= r + rb(i, 0) < p.level.r and 0 <= c + rb(i, 1) < p.level.c:
+            if p.level.dungeon[r + rr(i, 0)][c + rr(i, 1)] !=0 and p.level.dungeon[r + rr(i + 1, 0)][c + rr(i + 1, 1)] != 0 and p.fow[r + rr(i, 0)][c + rr(i, 1)] and p.fow[r + rr(i + 1, 0)][c + rr(i + 1, 1)]:
+                s.blit(pygame.transform.rotate(walls[2], 90 * i), (x, y))
+            if p.level.dungeon[r + rr(i, 0)][c + rr(i, 1)] == p.level.dungeon[r + rr(i + 1, 0)][c + rr(i + 1, 1)] == 0 and p.level.dungeon[r + rb(i,0)][c + rb(i,1)] != 0 and p.fow[r + rb(i,0)][c + rb(i,1)]:
+                s.blit(pygame.transform.rotate(walls[0], 90 * i), (x, y))
+
+def renderp(p, s, r):
+    s.fill((0,0,0))
+    for i in range(p.r, p.r + 2 * r + 1):
+        for j in range(p.c, p.c + 2 * r + 1):
+            if -1 < i - r < p.level.r and -1 < j - r < p.level.c and p.fow[i - r][j - r]:
+                if p.level.dungeon[i - r][j - r] == 0:
+                    render_wall(p, i-r, j-r, s, (j - p.c) * 32, (i - p.r) * 32)
+                elif p.level.dungeon[i - r][j - r] == 7:
+                    s.blit(door[0], ((j - p.c) * 32, (i - p.r) * 32))
+def ganimlist(p, animlist):
+    for i in p.toAnim:
+        for a in p.toAnim[i]:
+            animlist.append(Animate(a, animdict[i], 0, 0))
+    p.toAnim = {}
+def ranimlist(p, s, animlist, r):
+    i = 0
+    while i < len(animlist):
+        tmp = animlist[i].nxt()
+        if tmp == None:
+            animlist.pop(i)
+            continue
+        elif abs(animlist[i].cord[0]-p.r) <= dradius and abs(animlist[i].cord[1]-p.c) <= dradius:
+            s.blit(tmp, ((animlist[i].cord[1] + dradius - p.c)*32, (animlist[i].cord[0] + dradius - p.r)*32))
+        i += 1
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((672, 672))
+    screen.fill((0,0,0))
+    p = Player()
+    dradius = 10
+    playin = True
+    clock = pygame.time.Clock()
+    animlist = []
+
+    
+    
+    while playin:
+        renderp(p, screen, dradius)
+                    
+                    
+        screen.blit(player[0], (dradius * 32, dradius * 32))
+        ganimlist(p,animlist)
+        ranimlist(p,screen,animlist,dradius)
+        pygame.display.flip()
+        clock.tick(15)
+        p.cdd()
+        for e in pygame.event.get():
+            if e.type == pygame.locals.QUIT:
+                playin = False
+        pr = pygame.key.get_pressed()
+        for k in (ke for ke in p.movs if pr[ke]):
+                playin = binput(p, k)
+        
---
+++ m.py
@@ -0,0 +1,43 @@
+from random import sample
+class B:
+    def __init__(self,r,c,m):
+        self.g=0
+        self.r=r
+        self.c=c
+        self.m=m
+        self.b=[0]*r*c
+        for i in sample(range(r*c),m):
+            self.b[i]=-1
+            for j in(-c,0,c):
+                for k in(-1,0,1):
+                    self.b[i+j+k]+=1 if(k!=0 or j!=0)and-1<i+j<r*c and-1<k+i%c<c and self.b[i+j+k]>=0 else 0
+        self.v=[1]*r*c
+        self.l=r*c
+    def __str__(self):
+        return "\n".join(" ".join(str(j)if j<0 else" "+str(j)for j in self.b[i*self.c:(i+1)*self.c])for i in range(self.r))
+    def clk(self,r,c,t):
+        if self.v[self.c*r+c]==0:
+           if self.b[self.c*r+c]==sum(1 if(k!=0 or j!=0)and-1<r+j<self.r and-1<c+k<self.c and-1==self.v[self.c*(r+j)+c+k]else 0 for k in(-1,0,1)for j in(-1,0,1)):
+               for j in(-1,0,1):
+                   for k in(-1,0,1):
+                       if(k!=0 or j!=0)and-1<r+j<self.r and-1<c+k<self.c:
+                           if self.b[self.c*(r+j)+c+k]==-1:
+                               return-1
+                           else:
+                               self.dcs(r+j,c+k)
+        elif t and self.v[self.c*r+c]==1:
+            if self.b[self.c*r+c]==-1:
+                return-1
+            else:
+                self.dcs(r,c)
+        elif not t and self.v[self.c*r+c]!=0:
+            self.v[self.c*r+c]*=-1
+        return sum(0 if(j==-1 and-1==i)or(j==0 and-1!=j)else 1 for i,j in self.b,self.v)
+    def dsc(self,r,c):
+        if self.v[self.c*r+c]!=0:
+            self.v[self.c*r+c]=0
+            if self.b[self.c*r+c]==0:
+                for j in(-1,0,1):
+                    for k in(-1,0,1):
+                        if(k!=0 or j!=0)and-1<r+j<self.r and-1<c+k<self.c:
+                            self.dsc(r+j,c+k)
